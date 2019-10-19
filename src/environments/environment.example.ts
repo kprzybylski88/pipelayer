9658c1be867b2fee78c36143fb822570433b327d
KMP <krzysztof.mikolaj.przybylski@gmail.com>
2019-10-19 19:21:35 +0200

    done

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false, // true in environment.prod.ts
+  firebaseConfig: {
+    apiKey: 'apiKey',
+    authDomain: 'authDomain',
+    databaseURL: 'databaseUrl',
+    projectId: 'productId',
+    storageBucket: 'storageBucket',
+    messagingSenderId: 'messagingSenderId',
+    appId: 'appId'
+  },
+  appUrl: 'appUrl'
+};
