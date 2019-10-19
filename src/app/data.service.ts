5383f3fca6fd82e7da824101150cc54f8f612184
KMP <krzysztof.mikolaj.przybylski@gmail.com>
2019-10-19 19:31:24 +0200

    yup

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,9 +12,10 @@ export class DataService {
     private firestore: AngularFirestore,
   ) { }
   public generateCodes() {
+    const seed = Math.random();
     const md5 = new Md5();
-    const exKeyMd5 = md5.appendStr(Math.random().toString()).end();
-    const dotKeyMd5 = md5.appendStr(Math.random().toString()).end();
+    const exKeyMd5 = md5.appendStr(seed.toString()).end();
+    const dotKeyMd5 = md5.appendStr((seed + 1).toString()).end();
     return from(
       this.firestore.collection('games').add({
         game: {
